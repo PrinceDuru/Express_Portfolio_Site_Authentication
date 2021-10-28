f6d122e9dabfc0cf6026cfdc0060ff053b8bdc96
PrinceDuru <prince.i.duru@gmail.com>
2021-10-28 02:21:57 -0400

    modified code base to mvc model

---
+++ public/javascripts/script.js
@@ -0,0 +1,33 @@
+// File name: script.js
+// Name: Prince Duru
+// Student Id: 301167756
+// Date: 10/03/2021
+// ExpressPortfolio Site
+
+
+
+console.log('Goes to the client side.');
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=>{
+                if(!confirm("Are you sure?")) 
+                {
+                    event.preventDefault();
+                    window.location.assign('/contact-list');
+                }
+            });
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+})();
---
+++ server/routes/projects.js
@@ -0,0 +1,15 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('projects', { 
+    title: 'Projects',
+    userName: 'prince', 
+    displayName: req.user ? req.user.displayName : ''
+   });
+});
+
+module.exports = router;
